3e9e47793dc9f7aaa5cf8f988abd3e88127f352c
Sally Sheimo <sallysheimo@Sallys-MacBook-Pro-2.local>
2019-07-05 21:13:25 -0400

    Created Index Js and Store Js and installed some dependencies

---
+++ client/index.js
@@ -0,0 +1,24 @@
+/**
+ * ************************************
+ *
+ * @module  index.js
+ * @author
+ * @date
+ * @description entry point for application.  Hangs React app off of #contents in index.html
+ *
+ * ************************************
+ */
+
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './store';
+// import styles from './scss/application.scss'; need to create CSS file
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
