4b516d2084bcbb567be4ec71891396d618b7a4c5
Sally Sheimo <sallysheimo@Sallys-MacBook-Pro-2.local>
2019-07-06 11:28:16 -0400

    Main Container Creation

---
+++ client/components/App.jsx
@@ -0,0 +1,13 @@
+// main App wrapper
+
+import react from 'react';
+import Wrapper from '.containers/MainContainer';
+
+const App = () => {
+    <div id='App'>
+        <Wrapper />
+    </div>
+
+}
+
+export default App;
