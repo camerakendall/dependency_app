746ccd3f9446b5d61bab069233dedbfc3b8e5627
Camera <camerawalrond@gmail.com>
2019-07-06 16:06:16 -0400

    Starting server work

--- server/server.js
+++ server/server.js
@@ -4,12 +4,24 @@ const path = require('path'); //use path for correct directions
 const bodyParser = require('body-parser');
 const controller = require('./psql-controller.js');
 const server = app.listen(8080);
+const port = 8080
 
-// app.get()
+//app.use
+// app.use('/static', express.static(path.join(__dirname, 'public')))
+
+//need /build
+//need get request for / route on 3000
+//process variables (if production get /build. if development )
 
 
-// server.listen(8080, () => { //PROBABLY NEEDS TO CHANGE A BIT
-//     console.log('listening at http://localhost:8080');
-// });
+app.get('/', (req, res) =>{
+  res.sendFile(path.join(__dirname, 'test.html'));
+  
+
+})
+
+// app.get('/result' , controller.getDeps)
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = server;
