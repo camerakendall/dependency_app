c5130c896ae4dc52d5f56f038cdac84fc2ad1acc
Kwadwo <addoasa94@gmail.com>
2019-07-07 20:11:01 -0400

    Adds questions and small logic to stop counter at the last question

---
+++ client/ourQuestions.js
@@ -0,0 +1,20 @@
+export const ourQuestions = [
+  "Will you be using a Mac Operating system?",
+  "Are you going to be building a Full-Stack application?",
+  "Awesome! Will you be using Node.js for your Backend?",
+  "Do you want to use Express with your Node.js?",
+  "Would you like to install Nodemon?",
+  "Will you be utilizing a database?",
+  "Are you going to use a SQL database?",
+  "Do you want to use Postgresql as your SQL database?",
+  "Do you want to use a NoSQL database?",
+  "Would you like to use MongoDB as your NoSQL database?",
+  "Do you want to use Mongoose with MongoDB?",
+  "Do you want to install body-parser middle-ware for any POST requests you make?",
+  "Almost done - Will you be using React for your Frontend?",
+  "Are you going to use Redux for your React state management?",
+  "Last question! Do you wish to bundle your app with Webpack?",
+  "... All Done! Click the sumbit button below to obtain your dependecies.",
+];
+
+ 
